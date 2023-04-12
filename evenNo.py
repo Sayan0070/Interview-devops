1bb461e490626a9c32ec23009097d3f4e64a8512
Sayan Bhattacharya <sayan.bhattacharya91@gmail.com>
2023-04-12 16:41:18 +0530

    modify in code

--- evenNo.py
+++ evenNo.py
@@ -3,13 +3,32 @@
 #from that list find only even no
 #print all list and even no
 #display all the sum
-L =[]
-c= []
-a = int(input("How many numbers you want to put: "))
+L = []
+c = []
+
+while True:
+    try:
+        a = int(input("How many numbers do you want to put? "))
+        if a > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Please enter a valid integer.")
+
 for i in range(a):
-    L.append(int(input("Type you numbers")))
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            L.append(num)
+            break
+        except ValueError:
+            print("Please enter a valid integer.")
+
 for i in L:
-    if i % 2==0:
+    if i % 2 == 0:
         c.append(i)
-print(c)
-print(sum(c))
+
+print("All numbers:", L)
+print("Even numbers:", c)
+print("Sum of even numbers:", sum(c))
